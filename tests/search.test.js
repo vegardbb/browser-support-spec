be5a94b77b590f0d23af3a80c57878649a1aeced
Vegard Bjerkli Bugge <vegabug@live.no>
2019-10-15 01:50:15 +0200

    Define search test

---
+++ tests/search.test.js
@@ -0,0 +1,15 @@
+const { find, getSupport } = require('../fake/fakeCanIUse');
+const search = require('../search');
+
+// case A
+it("searches for the keyword 'radius', the result containing 'border-radius'", () => {
+  const searchResult = search(find)('radius');
+  expect(searchResult).toEqual(expect.arrayContaining(['border-radius']));
+});
+
+// case D
+it("searches for the keyword 'none', the result shall be empty", () => {
+  const searchResult = search(find)('none');
+  expect(searchResult).not.toEqual(expect.arrayContaining(['border-radius']));
+  expect(searchResult).toHaveLength(0);
+});
