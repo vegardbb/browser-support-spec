10bd6983d1ddcb915285921666b7f134653d8161
Vegard Bjerkli Bugge <vegabug@live.no>
2019-10-16 20:46:36 +0200

    ToDo comment

--- list.js
+++ list.js
@@ -1,13 +1,6 @@
 const { getSupport } = require('caniuse-api');
 
 /**
-const CurrentRetro = styled.div`
-  position: absolute;
-  right: 10px;
-  bottom: ${props => (props.simple ? '18px' : '10px')};
-  height: 30px;
-  width: 30px;
-`;
-
-github integration api
-*/
+ * @returns Map of string:int, giving the full name of a browser
+ * Converts the map string:obj to string:int
+ */
