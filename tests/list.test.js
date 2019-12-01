80fe3fff2b731a5d9863ebae248684a30d69f882
Vegard Bjerkli Bugge <vegabug@live.no>
2019-12-01 05:03:06 +0100

    Implement test case F

--- tests/list.test.js
+++ tests/list.test.js
@@ -47,4 +47,16 @@ xit("gets the ID's and versions of supported web browsers for the features denot
 });
 
 // case F
-xit("gets the ID's and versions of supported web browsers for the features denoted in the string 'none flexbox'", () => { });
+xit("gets the ID's and versions of supported web browsers for the features denoted in the string 'none flexbox'", () => {
+  /**
+    * Expected output from list(getSupport)('none flexbox'):
+    * Same output as in B
+    */
+  const supported = list(getSupport)('none flexbox');
+  const browserKeys = Object.keys(supported);
+  expect(browserKeys).toHaveLength(16);
+  expect(browserKeys).toEqual(expect.arrayContaining(['and_chr', 'and_ff', 'and_qq', 'and_uc', 'android', 'baidu', 'chrome', 'edge', 'firefox', 'ie_mob', 'ios_saf', 'op_mini', 'op_mob', 'opera', 'safari', 'samsung']));
+  const browserVerisons = Object.values(supported);
+  expect(browserVerisons).toEqual(expect.arrayContaining(['75', '67', '1.2', '12.12', '4.4', '7.12', '21', '12', '28', '11', '7.0', 'all', '12.1', '12.1', '6.1', '4']));
+  expect(browserVerisons.length).toEqual(browserKeys.length);
+});
