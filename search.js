811ed08bf29b75a0149c116fc397fa89171227f8
Vegard Bjerkli Bugge <vegabug@live.no>
2019-12-01 02:16:41 +0100

    Rename function

--- search.js
+++ search.js
@@ -1,7 +1,5 @@
 const { find } = require('caniuse-api');
-// We may import other feature search APIs here...
 
-const search = findAPI => query => findAPI(query);
-module.exports = search;
-exports.searchCanIUseAPI = search(find);
-// ...And export other query functions using them here...
+const getSearch = findAPI => query => findAPI(query);
+module.exports = getSearch;
+exports.searchCanIUse = getSearch(find);
