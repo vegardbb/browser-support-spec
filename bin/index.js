e7d1555bcd8bf5eb69240727879cdb610b5f8c9a
Vegard Bjerkli Bugge <vegabug@live.no>
2019-12-01 02:18:37 +0100

    Define bin directory

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+// Main file for the project
+// const { createServer } = require('http');
+// const path = require('path');
+// const { readFileSync } = require('fs');
+
+// todo: Parse a set of commands from
+const cliArguments = process.argsv;
+
+console.log(cliArguments);
+
