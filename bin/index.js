f4bed39b32693b52580774c55c92bd286a714809
Vegard Bjerkli Bugge <vegabug@live.no>
2019-12-20 18:33:16 +0100

    Implement print command

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const path = require('path');
+const { writeFileSync } = require('fs');
 const { find, getBrowserScope, getSupport } = require('caniuse-api');
 
 const browserNames = require('./shortNamesMap');
@@ -11,6 +13,11 @@ const { version } = require('../package.json');
 
 const printSearchResult = q => [].concat(search(find)(q)).reduce((text, name) => `${text}${name}${'\n'}`, '').trimEnd();
 const printListResult = q => Object.entries(list(getSupport)(q)).reduce((text, [id, num]) => `${text}${browserNames.get(id)}: ${num}${'\n'}`, '').trimEnd();
+
+// assuming fsPrint is a blocking function
+// printBrowserSpec() read in the object, then create a string, then print that string to a file in the 'current directory' - `${process.cwd()}${path.sep}.browserslistrc`
+//
+const printBrowserSpec = (path, browserObject) => writeFileSync(path, Object.entries(browserObject).reduce((text, [id, num]) => `${text}${'\n'}${id} >= ${num}`, '> 1%'));
 
 // Example: ['search', 'fetch']
 const cliArguments = process.argv.slice(2);
@@ -35,4 +42,11 @@ if (action === 'scope') {
   console.log(printSearchResult(verbArgs));
 } else if (action === 'list') {
   console.log(printListResult(verbArgs));
+} else if (action === 'print') {
+  try {
+    printBrowserSpec(`${process.cwd()}${path.sep}.browserslistrc`, list(getSupport)(verbArgs));
+    console.log('Printed the browserspec');
+  } catch {
+    console.log('Printing the browserspec failed');
+  }
 }
